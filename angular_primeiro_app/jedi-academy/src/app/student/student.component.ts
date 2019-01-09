80e01be0c2c4ca6b48bcc92c3c893749a92c5451
yanSilv <yansiba84@gmail.com>
2019-01-09 20:21:00 -0300

    Inserindo informações no HTML

--- angular_primeiro_app/jedi-academy/src/app/student/student.component.ts
+++ angular_primeiro_app/jedi-academy/src/app/student/student.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'jad-student',
@@ -7,8 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentComponent implements OnInit {
 
-  name:string = "Luke";
-  isJedi: boolean = true;
+  @Input() name:string;
+  @Input() isJedi: boolean;
   
   constructor() { }
 
