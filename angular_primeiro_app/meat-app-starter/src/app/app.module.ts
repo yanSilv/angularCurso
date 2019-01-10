1243c3d0c44e3c1a652d8cbb342e910b3ee73b77
Yan Santos <yan.santos@prodeb.ba.gov.br>
2019-01-10 11:32:21 -0300

    Desenvolvendo router

--- angular_primeiro_app/meat-app-starter/src/app/app.module.ts
+++ angular_primeiro_app/meat-app-starter/src/app/app.module.ts
@@ -3,23 +3,27 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
+import { ROUTES } from 'app/app.routes'
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component'
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component'
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    RouterModule
+    RouterModule.forRoot(ROUTES)
   ],
+  exports: [ RouterModule ],
   providers: [],
   bootstrap: [AppComponent]
 })
