1375ef028c1c7a21039c2334d80f3ac11fa1b73f
Yan Santos <yan.santos@prodeb.ba.gov.br>
2019-01-11 10:14:20 -0300

    Consumindo serviço http

--- angular_primeiro_app/meat-app-starter/src/app/restaurants/restaurants.service.ts
+++ angular_primeiro_app/meat-app-starter/src/app/restaurants/restaurants.service.ts
@@ -1,28 +1,18 @@
-import { Restaurant } from './restaurant/restaurant.model'
+import { Injectable } from '@angular/core'
+import { Http } from '@angular/http'
 
+import { Restaurant } from './restaurant/restaurant.model'
+import { MEAT_API } from 'app/app.appi'
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map'
+
+@Injectable()
 export class RestaurantsService {
 
-    constructor(){}
-    rests: Restaurant[] = [
-        {
-          id: "bread-bakery",
-          name: "Bread & Bakery",
-          category: "Bakery",
-          deliveryEstimate: "25m",
-          rating: 4.9,
-          imagePath: "assets/img/restaurants/breadbakery.png"
-        },
-        {
-          id: "burger-house",
-          name: "Burger House",
-          category: "Hamburgers",
-          deliveryEstimate: "100m",
-          rating: 3.5,
-          imagePath: "assets/img/restaurants/burgerhouse.png"
-        }
-      ];
+    constructor(private http: Http ){}
 
-    restaurants(): Restaurant [] {
-        return this.rests;
+    restaurants(): Observable<Restaurant[]> {
+        return this.http.get(`${MEAT_API}/restaurants`)
+                        .map(response => response.json());
     }
 }
