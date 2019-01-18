e8cee523c1bef2865e7e33e50003bdcadf29d2d1
Yan Santos <yan.santos@prodeb.ba.gov.br>
2019-01-18 10:19:24 -0300

    Começando o desenvolvimento do carrinho de compras

--- angular_primeiro_app/meat-app-starter/src/app/app.module.ts
+++ angular_primeiro_app/meat-app-starter/src/app/app.module.ts
@@ -18,6 +18,7 @@ import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
 import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
+import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service'
 
 
 @NgModule({
@@ -40,7 +41,10 @@ import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
     RouterModule.forRoot(ROUTES)
   ],
   exports: [ RouterModule ],
-  providers: [RestaurantsService],
+  providers: [
+    RestaurantsService,
+    ShoppingCartService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
