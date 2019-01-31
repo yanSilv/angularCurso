9de2f291b7ddadc234408445f8e6a86f8928869b
Yan Santos <yan.santos@prodeb.ba.gov.br>
2019-01-31 10:38:35 -0300

    Component About como module

--- angular_primeiro_app/meat-app-starter/src/app/app.routes.ts
+++ angular_primeiro_app/meat-app-starter/src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from "@angular/router"
 
 import { HomeComponent } from "./home/home.component";
-import { AboutComponent } from "./about/about.component"
 import { RestaurantsComponent } from "app/restaurants/restaurants.component"
 import { RestaurantDetailComponent } from "./restaurant-detail/restaurant-detail.component";
 import { MenuComponent } from "./restaurant-detail/menu/menu.component";
@@ -11,7 +10,7 @@ import { OrderSummaryComponent } from "./order-summary/order-summary.component";
 
 export const ROUTES: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'about', component: AboutComponent},
+    {path: 'about', loadChildren: './about/about.module#AboutModule'},
     {path: 'restaurants', component: RestaurantsComponent},
     {path: 'restaurants/:id', component: RestaurantDetailComponent,
         children: [
