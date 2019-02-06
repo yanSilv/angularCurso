a7517a6ffe0610f7ed4dea5d7b97bb25d4eb1796
Yan Santos <yan.santos@prodeb.ba.gov.br>
2019-02-06 09:39:33 -0300

    Criando a animação do SnackBar PT 3

--- angular_primeiro_app/meat-app-starter/src/app/shared/messages/snackbar/snackbar.component.ts
+++ angular_primeiro_app/meat-app-starter/src/app/shared/messages/snackbar/snackbar.component.ts
@@ -3,6 +3,8 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
 import { NotificationService } from '../notification.service';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/timer';
+import 'rxjs/add/operator/do'
+import 'rxjs/add/operator/switchMap'
 
 @Component({
   selector: 'mt-snackbar',
@@ -32,11 +34,11 @@ export class SnackbarComponent implements OnInit {
   constructor(private noificationService: NotificationService) { }
 
   ngOnInit() {
-    this.noificationService.notifier.subscribe ( message=> {
+    this.noificationService.notifier.do ( message=> {
       this.message = message
       this.snackVisibility = 'visible'
-      Observable.timer(3000).subscribe(timer => this.snackVisibility = 'hidden');
-    });
+    }).switchMap(message => Observable.timer(3000))
+      .subscribe(timer => this.snackVisibility = 'hidden');
   }
 
 }
