d3a2a2acd81764d1e84a6a6353657d72549ef33a
Yan Santos <yan.santos@prodeb.ba.gov.br>
2019-02-06 14:01:34 -0300

    Animação adicionar item no carrinho

--- angular_primeiro_app/meat-app-starter/src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
+++ angular_primeiro_app/meat-app-starter/src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
@@ -1,12 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { ShoppingCartService } from './shopping-cart.service';
+import { trigger, state, style, transition, animate } from '@angular/animations' 
+import { keyframes } from '@angular/animations';
 
 @Component({
   selector: 'mt-shopping-cart',
-  templateUrl: './shopping-cart.component.html'
+  templateUrl: './shopping-cart.component.html',
+  animations: [
+    trigger('row', [
+      state('realy', style({opacity: 1})),
+      transition('void => realy', animate('300ms 0s ease-in', keyframes([
+        style({opacity: 0, transform: 'translateX(-30px)', offset:0}),
+        style({opacity: 0.8, transform: 'translateX(10px)', offset:0.8}),
+        style({opacity: 1, transform: 'translateX(0px)', offset:1})
+      ]))),
+      transition('realy => void', animate('300ms 0s ease-out', keyframes([
+        style({opacity: 1, transform: 'translateX(0px)', offset:0}),
+        style({opacity: 0.8, transform: 'translateX(-10px)', offset:0.2}),
+        style({opacity: 0, transform: 'translateX(30px)', offset:1})
+      ])))
+    ])
+  ]
 })
 export class ShoppingCartComponent implements OnInit {
 
+  rowState = 'realy';
   constructor(private shoppingCartService: ShoppingCartService) { }
 
   ngOnInit() {
